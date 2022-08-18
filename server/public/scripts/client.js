ce9c6e3b2a3971a0c735216062ccf8c24055096c
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-08-18 17:58:12 -0500

    up to date with the group

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,123 @@
-console.log( 'js' );
+console.log('js');
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
+$(document).ready(function () {
+  console.log('JQ');
   // Establish Click Listeners
-  setupClickListeners()
+  setupClickListeners();
+  $('body').on('click', '.delete-koala', deleteKoala);
+
   // load existing koalas on page load
   getKoalas();
-
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+  $('.delete-koala').on('click', deleteKoala);
+  $('#addButton').on('click', function () {
+    console.log('in addButton on click');
+
+    if ($('#genderIn').val() === '') {
+      alert('Choose Male or Female');
+      return;
+    } else if ($('#readyForTransferIn').val() === '') {
+      alert('Choose True or False');
+      return;
+    } else {
+      let koalaToSend = {
+        name: $('#nameIn').val(),
+        age: $('#ageIn').val(),
+        gender: $('#genderIn').val(),
+        ready: $('#readyForTransferIn').val(),
+        notes: $('#notesIn').val(),
+      };
+      // call saveKoala with the new obejct
+      saveKoala(koalaToSend);
+      emptyInputs();
+
+
+    }
+  });
 }
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
-  
+function deleteKoala() {
+  console.log('inDelete koala');
+  const koalaId = $(this).data('id');
+  $.ajax({
+    type: 'DELETE',
+    //deleting koala in /koals using koalaId
+    url: `/koalas/${koalaId}`
+  }).then(function (response) {
+    console.log(response);
+    getKoalas();
+  }).catch(function (error) {
+    console.log(error);
+    alert('something went wrong');
+  });
+
+}
+
+function getKoalas() {
+  console.log('in getKoalas');
+  // ajax call to server to get koalas - GET
+  $.ajax({
+    type: 'GET',
+    url: '/koalas'
+  }).then(function (response) {
+    console.log(response);
+    $('#viewKoalas').empty();
+    for (let koala of response) {
+      // ID from database for delete button
+      console.log(`${koala.ready}`);
+      if (`${koala.ready}` === 'false') {
+        $('#viewKoalas').append(`
+        <tr>
+        <td>${koala.id}</td>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.ready}</td>
+        <td>${koala.notes}</td>
+        <td></td>
+        <td>
+          <button class="delete-koala" data-id="${koala.id}">Delete</button>
+        <td>
+      </tr>
+        `);
+      } else {
+        $('#viewKoalas').append(`
+        <tr>
+        <td>${koala.id}</td>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.ready}</td>
+        <td>${koala.notes}</td>
+        <td><button class="ready-transfer" datat id="${koala.ready}">Ready for Transfer</button></td>
+        <td>
+          <button class="delete-koala" data-id="${koala.id}">Delete</button>
+        <td>
+      </tr>
+        `);
+      }
+    }
+  }).catch(function (error) {
+    console.log(error);
+    alert('Something went wrong!')
+  })
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
- 
+function saveKoala(newKoala) {
+  console.log('in saveKoala', newKoala);
+  $.ajax({
+    type: 'POST',
+    url: '/koalas',
+    data: newKoala
+  }).then(function (response) {
+    console.log(response);
+    getKoalas();
+  });
+}
+
+function emptyInputs() {
+  $('input').val('');
+  $('select').val('');
 }
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,14 +1,115 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pool = require('../modules/pool.js')
+
+const koalas = [
+    {
+        name: 'Scotty',
+        age: 4,
+        gender: 'M',
+        ready: 'true',
+        notes: 'Born in Guatemala'
+    },
+    {
+        name: 'Jean',
+        age: 5,
+        gender: 'F',
+        ready: 'true',
+        notes: 'Allergic to lots of lava'
+    },
+    {
+        name: 'Ororo',
+        age: 7,
+        gender: 'F',
+        ready: 'false',
+        notes: 'Loves listening to Paula (Abdul)'
+    },
+    {
+        name: 'Logan',
+        age: 15,
+        gender: 'M',
+        ready: 'false',
+        notes: 'Loves the sauna'
+    },
+    {
+        name: 'Charlie',
+        age: 9,
+        gender: 'M',
+        ready: 'true',
+        notes: 'Favorite band is Nirvana'
+    },
+    {
+        name: 'Betsy',
+        age: 4,
+        gender: 'F',
+        ready: 'true',
+        notes: 'Has a pet iguana'
+    }
+];
 
 // DB CONNECTION
+koalaRouter.get('/', (req, res) => {
+    console.log('in GET /koalas');
+    //Specify which query to run
+    //queryText = SELECT in SQL
+    const queryText = 'SELECT * FROM "koala_table";'
+    pool.query(queryText).then((result) => {
+        console.log('SELECT FROM SUCCESS', result);
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('Error in GET /koals', error);
+        res.sendStatus(500);
+    });
+})
+
 
 
 // GET
-
+// koalaRouter.get('/', (req, res) => {
+//     res.send(koalas);
+// })
 
 // POST
 
+koalaRouter.post('/', (req, res) => {
+    const koala = req.body;
+    const queryText = `INSERT INTO "koala_table"("name", "age", "gender", "ready", "notes")
+    VALUES ($1, $2, $3, $4, $5);`
+
+    pool.query(queryText, [koala.name, koala.age, koala.gender, koala.ready, koala.notes])
+        .then((results) => {
+            console.log(results);
+            res.send(results);
+        }).catch((error) => {
+            console.log('Error in POST /koalas', error);
+            res.sendStatus(500);
+        });
+
+});
+
+koalaRouter.delete('/:id', (req, res) => {
+    const koalaId = req.params.id;
+    console.log('DELETE /koalas', koalaId);
+    const queryText = 'DELETE FROM "koala_table" WHERE "id" = $1;';
+    pool.query(queryText, [koalaId])
+    .then((result) => {
+        res.sendStatus(200);//successfully deleted something
+    }).catch((error) => {
+        console.log('ERROR', error);
+        res.sendStatus(500);
+    })
+})
+
+
+
+
+// koalaRouter.post('/', (req, res) => {
+//     const koala = req.body;
+//     console.log(req.body);
+//     koalas.push(koala);
+//     res.send(koala);
+//     // res.sendStatus(201);
+//   })
 
 // PUT
 
